6b93270218cc4b4fb9bbb971b05d1917e3b96abd
JeanGoumez <adrielgo.mez123@gmail.com>
2023-04-03 20:22:26 -0500

    Mi primer commit, creacion de proyecto nextjs con tailwind

---
+++ pages/_app.js
@@ -0,0 +1,5 @@
+import '@samplenextjs/styles/globals.css'
+
+export default function App({ Component, pageProps }) {
+  return <Component {...pageProps} />
+}
---
+++ pages/principal.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Faq = () => {
+  return (
+    <>
+      <div className="bg-blue-200 px-4">xd</div>
+      <div name="test1" className="w-full max-w-6xl mx-auto mb-10 lg:mb-32">
+        <h1 className="bg-red-500 px-4">gumball</h1>
+      </div>{" "}
+    </>
+  );
+};
+export default Faq;
